f1a4a16141220d278267b6d5088b335bb07272f8
Gabriel Caetano <gabrielvscaetano@gmail.com>
2022-12-11 07:08:16 -0300

    feat: sports table model and seeder

---
+++ app/Models/Sport.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Tournament from './Tournament'
+
+export default class Sport extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public sofascoreId: number
+
+  @column()
+  public name: string
+
+  @column()
+  public slug: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasMany(() => Tournament)
+  public tournaments: HasMany<typeof Tournament>
+}
