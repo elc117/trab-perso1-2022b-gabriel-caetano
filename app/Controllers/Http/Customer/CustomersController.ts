15fc2b4f6765bffed577e9163277ec0e3d5beacc
Gabriel Caetano <gabrielvscaetano@gmail.com>
2023-01-09 10:48:07 -0300

    feat: inactive user

--- app/Controllers/Http/Customer/CustomersController.ts
+++ app/Controllers/Http/Customer/CustomersController.ts
@@ -1,10 +1,16 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Customer from 'App/Models/Customer'
+import { DateTime } from 'luxon'
 
 export default class CustomersController {
   public async index({ auth }: HttpContextContract) {
+    const yesterday = DateTime.now().plus({ days: -1 })
     const user = auth.user!
     const customer = await Customer.findOrFail(user.id)
+    if (customer.active && customer.planDueDate < yesterday) {
+      customer.active = false
+      await customer.save()
+    }
     return {
       email: user.email,
       name: customer.name,
