e942161273c17722efe59fce0629044178c66ff2
Gabriel Caetano <gabrielvscaetano@gmail.com>
2023-01-11 21:08:35 -0300

    feat: return league with county in slug

--- app/Controllers/Http/Customer/LeaguesController.ts
+++ app/Controllers/Http/Customer/LeaguesController.ts
@@ -3,17 +3,24 @@ import League from 'App/Models/League'
 
 export default class LeaguesController {
   public async index() {
-    const leagues = await League.query()
-    return leagues.map((league) => {
+    const leagues = await League.query().preload('country')
+    const leaguesRes = leagues.map((league) => {
       return {
         id: league.id,
         sportId: league.sportId,
         countryId: league.countryId,
         name: league.name,
-        slug: league.slug,
+        slug: `${league.slug}-${league.country.slug}`,
         logoUrl: league.logoUrl,
         active: league.active,
       }
     })
+    let slugs = leaguesRes.map((x) => x.slug)
+    slugs = [...slugs, slugs.at(1)]
+    const slugsSet = new Set(slugs)
+    console.log(slugs.length)
+    console.log([...slugsSet].length)
+
+    return leaguesRes
   }
 }
